035813f894fb01545302231a3bf6730c768009c4
bbgfarmer <80210817+NotDaniDev@users.noreply.github.com>
2022-12-10 11:59:28 +0530

    Update 3 idiots project.py

--- 3 idiots project.py
+++ 3 idiots project.py
@@ -1,87 +1,128 @@
-lst = ['Aditya','Rathiin', "Ihit", "Vivyan", 'Ram', 'Mohammed', 'Sunita', 'Aarav', 'Arnav', 'Ayaan']
-lowerlist = []
-upperlist = []
-
-print("Options:")
-print("(0) Exit")
-print("(1) Enter an 11th name")
-print("(2) Count the number of vowels in each name")
-print("(3) If there is an incorrect name, replace it with an new one.")
-print("(4) Find the middle characters")
-print("(5) Convert all the names to Title Case. (Uppercase)")
-print("(6) Calculate how many names start or end with 'a'")
-
-def function1():
-    print("Duplicate names will not be accepted.")
-    errorCount = 0
-    while errorCount < 3:
-        y = input("Enter an 11th name: ")
-        for i in range(len(lst)):
-            lowerlist.append(lst[i])
-            lowerlist[i] = lst[i].lower()
-        if y in lowerlist:
-            print("Duplicate values are not allowed")
-            errorCount+=1
+Mcard = {}
+Gcard = {}
+names = []
+marks = []
+grade = []
+maths = []
+cs = []
+eng = []
+def float_input(input_message, error_message):
+  while True:
+    try:
+      f = float(input(input_message))
+      break
+    except:
+      print(error_message) # or `pass` for nothing
+  return f
+# cont = True is used to keep the program running. It's set to true or false at the end of the program.
+cont = True
+while cont:
+    # Checks if the number of students is an integer
+    try:
+        # Number of students in the list
+        num = int(input("Please input the number of students in your class: "))
+        print(" ")
+        # Names of each of the students in the list
+        names = [str(i) for i in input("Please input all the names seperating them with a comma and space (Example: Aditya, Rathiin, Vivyan)").split(", ")]
+        # Checks the number of names entered is the same as the number of students initially decided
+        if len(names)!=num:
+            print(" ")
+            print("The number of names don't match the number of names you've given (Did you seperate the names with a comma and space?)")
+        # Main portion of the code.
         else:
-            lst.append(y)
-            print(lst)
+            # Checks to see if the marks are entered as a number.
+            try:
+                # Used to create the dictionary for the grades and average.
+                for i in range(0, len(names)):
+                    print(" ")
+                    m = float_input(f"please input the Maths percentage of {names[i]}: ", 'Not a number')
+                    print(" ")
+                    im = int(m) + 1
+                    c = float_input(f"Please input the Computer Science percentage of {names[i]}: ", 'Not a number')
+                    print(" ")
+                    ic = int(c) + 1
+                    e = float_input(f"Please enter the English percentage of {names[i]}: ", 'Not a number')
+                    ie = int(e) + 1
+                    # Checks if the numbers are greater or equal to 0 and lesser or equal to 100.
+                    if im in range(0,101) and ic in range(0,101) and ie in range(0,101):
+                        avgMarks = ((m + c + e) / 3)
+                        maths.append(m)
+                        cs.append(c)
+                        eng.append(e)
+                        if int(avgMarks) + 1 in range(90,101):
+                            grade.append('A')
+                        elif int(avgMarks) + 1 in range(80, 90):
+                            grade.append('B')
+                        elif int(avgMarks) + 1 in range(70,80):
+                            grade.append('C')
+                        elif int(avgMarks) + 1 in range(60,70):
+                            grade.append('D')
+                        elif int(avgMarks) + 1 in range(0,60):
+                            grade.append("Fail")
+                        # Prints the error for the if condition.
+                    else:
+                        print(" ")
+                        print("The number has to be above 0 and below 100.")
+                    # Adds the average marks of the class to a list to be used in a sum for calculating the overall average of the class.
+                    marks.append(avgMarks)
+                    # Creates the dictionary for the Marks Card.
+                    Mcard.update({names[i]: {"Maths": maths[i],"Computer science": cs[i], "English": eng[i]}})
+                    # Creates the grade card and adds the overall grade to it as a dictionary.
+                    Gcard.update({names[i]: grade[i]})
+                print("-------------------------------------------")
+                print("The subject-wise percentage for each student is:")
+                # Prints the subject-wise percentage for each student.
+                for x, y in Mcard.items():
+                    print("{}: {}".format(x, y))
+                print("-------------------------------------------")
+                print("The overall grade for each student is:")
+                # Prints the grades for each student.
+                for x, y in Gcard.items():
+                    print("{}: {}".format(x, y))
+                print("-------------------------------------------")
+                print("the average percentage of the entire class is: ")
+                # Calculates and prints the average percentage of the entire class.
+                print(round(sum(marks) / len(names), 2))
+                print("-------------------------------------------")
+            # Except for the number check.
+            except:
+                print(" ")
+                print("The input must be a number.")
+    # Except for the number of students integer check.
+    except:
+        print(" ")
+        print("The number of students has to be an integer.")
+    # Loops the continuation statement to be able to reinput the answer.
+    while True:
+        print(" ")
+        # Variable to see if the user wants to continue or not
+        x = input("Do you wish to continue? (Y/N)")
+        # Checks if the input is yes and removes case sensitivity.
+        if x.lower() == 'y' or x.lower() == 'yes':
+            print(" ")
+            print("Continuing...")
+            print(" ")
+            print(" ")
+            # Resets the lists to empty.
+            Mcard = {}
+            Gcard = {}
+            names = []
+            marks = []
+            grade = []
+            maths = []
+            cs = []
+            eng = []
+            # Breaks the while loop used to reinput the answer
             break
-def function2():
-    print("This will count the vowels in each name.")
-    for i in range(len(lst)):
-        lowerlist.append(lst[i])
-        lowerlist[i] = lst[i].lower()
-        count = lowerlist[i].count('a') + lowerlist[i].count('e') + lowerlist[i].count('i') + lowerlist[i].count('o') + \
-                lowerlist[i].count('u')
-        print("The number of vowels in", lst[i], "is", count)
-def function3():
-    print(lst)
-    Name = input("Enter the name you want to replace: ")
-    ReplacedName = input("Enter the replacement name: ")
-    for i in range(len(lst)):
-        lowerlist.append(lst[i])
-        lowerlist[i] = lst[i].lower()
-        if Name.lower() == lowerlist[i]:
-            Name = (Name[0].upper() + Name[1:])
-            lst.remove(Name)
-            lst.insert(i, ReplacedName)
-            print(lst)
+        # Checks if the input is no and removes case sensitivity.
+        elif x.lower() == 'n' or x.lower() == 'no':
+            print(" ")
+            print("Okay, closing...")
+            print(" ")
+            print(" ")
+            # Sets the cont variable to false to effectively stop the program by stopping the while cont loop the entire program is under.
+            cont = False
             break
-    else:
-        print("The name is not in the list.")
-def function4():
-    for i in range(len(lst)):
-        if len(lst[i]) == 4:
-            print("The middle characters of", lst[i], "are:", lst[i][1:-1])
-        elif len(lst[i]) == 3:
-            print("The middle characters of", lst[i], "are:", lst[i][1:-1])
-        elif len(lst[i]) % 2 == 0 and len(lst[i]) != 3 and len(lst[i]) != 4:
-            print("The middle characters of", lst[i], "are:",
-                  lst[i][((len(lst[i]) - 1) // 2):(((len(lst[i]) - 1) // 2) * 2)])
+        # If the input is not yes or no, it shows this error.
         else:
-            print("The middle characters of", lst[i], "are:", lst[i][(len(lst[i]) // 2 - 1):(len(lst[i]) // 2 + 2)])
-def function5():
-    for i in range(len(lst)):
-        upperlist.append(lst[i])
-        upperlist[i] = lst[i].capitalize()
-    print(upperlist)
-def function6():
-    for i in range(len(lst)):
-        lowerlist.append(lst[i])
-        lowerlist[i] = lst[i].lower()
-        if lowerlist[i][0:1] == 'a' or lowerlist[i][len(lst[i]) - 1] == 'a':
-            print(lst[i], "starts or ends with 'a'.")
-        else:
-            print(lst[i], "does not start or end with 'a'.")
-
-functions = [function1, function2, function3, function4, function5, function6]
-
-while True:
-    x = int(input("Enter your choice: "))
-    if x in range(1, 6):
-        functions[x-1]()
-    elif x == 0:
-        print("now exiting the program")
-        break
-    else:
-        print("Invalid input")
+            print("The input has to be yes or no.")
